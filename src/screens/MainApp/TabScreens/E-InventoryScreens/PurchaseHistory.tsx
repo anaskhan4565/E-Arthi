2e8d5ae6c4f897af3b7ab6353356dc121f6d9261
Shehryar123-alpha <120850.muhammad@alpha.edu.pk>
2025-01-12 19:01:51 +0500

    Purchase History done

--- src/screens/MainApp/TabScreens/E-InventoryScreens/PurchaseHistory.tsx
+++ src/screens/MainApp/TabScreens/E-InventoryScreens/PurchaseHistory.tsx
@@ -1,31 +1,116 @@
-import React from 'react';
-import type { PropsWithChildren } from 'react';
+import React from "react";
+import type { PropsWithChildren } from "react";
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-} from 'react-native';
-import { useTranslation } from 'react-i18next';
-
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
+import { useTranslation } from "react-i18next";
+import { fonts } from "../../../../../util/FontName";
+import PurchaseHistory from "../../../../../util/PurchaseHistory";
+import Navbar from "../../Navbar/Navbar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import colors from "../../../../../util/colors";
+import CustomSearchApp from "../../CustomComponent/CustomSearchApp";
 
 function PurchaseHisotry(): React.JSX.Element {
-    const {t} = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>{t('Screen')}</Text>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <Navbar />
+      <View style={{ flex: 7 }}>
+        <View style={styles.searchbar}>
+          <CustomSearchApp placeholder={"Search in here"} />
+        </View>
+        <View style={{ marginBottom: hp(1.2),marginTop: hp(0),marginHorizontal: wp(5), }}>
+          <Text style={{ fontFamily: fonts.SemiBold, fontSize: hp(2.4) }}>
+            Purchase History
+          </Text>
+        </View>
+        <View style={styles.Header}>
+          <Text style={styles.HeaderCol}>Item {"\n"}Description {"\n"}& Qty</Text>
+          <Text style={[styles.HeaderCol,{textAlign: "center"}]}>Cost</Text>
+          <Text style={styles.HeaderCol}>Date Order Placed</Text>
+        </View>
+        {PurchaseHistory.map(
+          (data, index) =>
+            data.desc.trim() !== "" && (
+              <View style={styles.row} key={index}>
+                <View style={styles.decsAndQty}>
+                  <Text style={styles.decs}>{data.desc}</Text>
+                  <Text style={styles.decs}>{data.qty}</Text>
+                </View>
+                <Text style={styles.cost}>{data.cost}</Text>
+                <Text style={styles.date}>{data.date}</Text>
+              </View>
+            )
+        )}
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+  searchbar : {
+    marginTop: hp(1.3),
+    height: hp('7%'),
+
+  },
+  Header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: wp(4),
+    height: hp(10)
+  },
+  HeaderCol: {
+    fontFamily: fonts.SemiBold,
+    fontSize: hp(2),    
+    width: wp(29),
+    textAlign: "left"
+    
+  },
+  cost: {
+    fontFamily: fonts.Regular,
+    fontSize: hp(1.8),
+    textAlign: "center",
+    width: wp(33),
+  },
+  date: {
+    fontFamily: fonts.Regular,
+    fontSize: hp(1.8),
+    width: wp(30)
+  },
+  decs: {
+    fontFamily: fonts.Regular,
+    fontSize: hp(1.8),
+  },
+  decsAndQty: {
+    alignContent: "center",
+    width: wp(30),
+    fontFamily: fonts.Regular,
+    fontSize: hp(1.8),
+    
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: wp(4),
+    
+  },
 });
 
 export default PurchaseHisotry;

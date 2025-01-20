580f2edd59c1066980115f4811825b78e7c74b31
Shehryar123-alpha <shehryar11w@gmail.com>
2025-01-20 23:05:39 +0500

    Reserve Warehouse done

---
+++ util/WarehouseSpaceCategory.js
@@ -0,0 +1,7 @@
+export default [
+  { type: "Refrigerated Storage", space: "50,000 sq ft", price: "PKR 210,000" },
+  { type: "Frozen Cold Storage", space: "30,000 sq ft", price: "PKR 110,000"},
+  { type: "Silo Storage", space: "100,000 cubic ft", price: "PKR 51,000"},
+  { type: "Temperature Controlled Storage", space: "50,000 sq ft", price: "PKR 75,000"},
+  { type: "Dry Storage", space: "100,000 sq ft", price: "PKR 30,000" },
+];

016b2a3830552116b7008775e24f58d2993e6f09
Hadi Abbas <hadiabbas.official@gmail.com>
2025-01-25 16:23:38 +0500

    made changes to loan

--- src/screens/MainApp/EMandi/CustomComp/CustomPicker.jsx
+++ src/screens/MainApp/EMandi/CustomComp/CustomPicker.jsx
@@ -5,16 +5,28 @@ import colors from "../../../../../util/colors";
 import { useTranslation } from "react-i18next";
 import { Picker } from "@react-native-picker/picker";
 import { fonts } from "../../../../../util/FontName";
-const CustomPicker = ({w_given=hp(8), items, currentState, setCurrentState, isheader,hp_given=hp(5),min_given=wp(50),padding_f=false }) => {
+const CustomPicker = ({ w_given = hp(8),
+  allow_shadow = false, tx_color = colors.WHITE,
+  items, currentState, bg_color_on = true, setCurrentState,
+  stateName="BuyScreen",
+  isheader, hp_given = hp(5), min_given = wp(50), padding_f = false }) => {
   const { t } = useTranslation();
 
   const checkVal = (e) => {
-    if (currentState) {
+    if (stateName=='BuyScreen') {
       if (e === 0) {
         setCurrentState('Buy');
       } else {
         setCurrentState('Sell');
       }
+    }
+    else if(stateName=='CategoryVendor'){
+      if (e === 0) {
+        setCurrentState(0);
+      } else {
+        setCurrentState(1);
+      }
+
     }
   };
 
@@ -22,20 +34,34 @@ const CustomPicker = ({w_given=hp(8), items, currentState, setCurrentState, ishe
     <View
       style={[
         styles.detailRow,
-        { backgroundColor: isheader ? colors.DARK_GREEN : colors.LIGHT_GRAY,padding:!padding_f?hp(1):0 ,width:w_given},
+        {
+          backgroundColor: !bg_color_on
+            ? null
+            : isheader
+              ? colors.DARK_GREEN
+              : colors.LIGHT_GRAY,
+          padding: !padding_f ? hp(1) : 0,
+          width: w_given,
+        },
+        allow_shadow ? styles.shadowStyle : null, // Apply shadow conditionally
       ]}
     >
-    <View style={[styles.pickerContainer,{height:hp_given,minWidth:min_given}]}>
+      <View
+        style={[
+          styles.pickerContainer,
+          { height: hp_given, minWidth: min_given },
+        ]}
+      >
         <Picker
           mode="dropdown"
           itemStyle={[
             styles.pickerItem,
-            { fontFamily: fonts.SemiBold, fontWeight: 'bold'},
+            { fontFamily: fonts.SemiBold, fontWeight: 'bold' },
           ]}
           onValueChange={(e) => checkVal(e)}
           style={{
-            color: isheader ? colors.WHITE : colors.BLACK,
-            fontSize: hp(2.5)
+            color: isheader ? tx_color : colors.BLACK,
+            fontSize: hp(2.5),
           }}
         >
           {items.map((item, index) => (
@@ -49,26 +75,36 @@ const CustomPicker = ({w_given=hp(8), items, currentState, setCurrentState, ishe
         </Picker>
       </View>
     </View>
-  );
-};
+
+  )
+}
 
 export default CustomPicker;
-
 const styles = StyleSheet.create({
   detailRow: {
     flexDirection: 'row',
     alignItems: 'center',
+
     justifyContent: 'space-between',
     backgroundColor: colors.LIGHT_GRAY,
     borderRadius: hp(0.5),
-    //padding: hp('1%'),
+  },
+  shadowStyle: {
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+    borderRadius: hp(0.4),
+    elevation: hp(0.3), // Android shadow
   },
   pickerContainer: {
     borderColor: colors.LIGHT_GRAY,
     borderRadius: 8,
+
     justifyContent: 'center',
   },
   pickerItem: {
     height: hp('10%'),
   },
 });
+

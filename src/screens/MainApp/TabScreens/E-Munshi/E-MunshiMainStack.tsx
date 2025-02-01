2c13163a0708ef15a02fb986f9c7ff454beb3d5f
anaskhan4565 <anaskhan4565@gmail.com>
2025-02-01 18:22:03 +0500

    emunshi farmdetail page completed

--- src/screens/MainApp/TabScreens/E-Munshi/E-MunshiMainStack.tsx
+++ src/screens/MainApp/TabScreens/E-Munshi/E-MunshiMainStack.tsx
@@ -21,16 +21,18 @@ import i18next from "i18next";
 import EMunshi from "./E-Munshi"
 import EMunshiItemName from "./E-MunshiItemName"
 import EMunshiCashFlow from "./E-MunshiCashflow"
+import EMunshiFarmName from "./E-MunshiFarmName";
 const Stack = createNativeStackNavigator();
 
 
 function EMunshiMainStack(): React.JSX.Element {
     return (
         //  <I18nextProvider i18n={i18next}>
-                <Stack.Navigator initialRouteName={ScreensName.EMunshi} screenOptions={{ headerShown: false }}>
+                <Stack.Navigator initialRouteName={ScreensName.EMunshiFarmName} screenOptions={{ headerShown: false }}>
                     <Stack.Screen name={ScreensName.EMunshi} component={EMunshi} />
                     <Stack.Screen name={ScreensName.EMunshiItemName} component={EMunshiItemName} />
                     <Stack.Screen name={ScreensName.EMunshiCashFlow} component={EMunshiCashFlow} />
+                    <Stack.Screen name={ScreensName.EMunshiFarmName} component={EMunshiFarmName} />
                 </Stack.Navigator>
         // </I18nextProvider>
     );

dabc4a024e427c123a73c24d2fed640b4b622275
Hadi Abbas <hadiabbas.official@gmail.com>
2025-02-09 21:28:01 +0500

    fixed

--- util/E-Warehouse.js
+++ util/E-Warehouse.js
@@ -2,12 +2,12 @@ import ScreensName from './ScreensName.ts';
 export default [
     {
         screen: ScreensName.EWarehouseNewSpaceCropSelect,
-        title: "Find New Warehouse",
+        title: "Find Storage",
         img: require('../src/assets/MainApp/E-Inventory/Inventory.png')
     },
     {
         screen: ScreensName.EWarehousePreviousWarehouses,
-        title: "View Warehouse History",
+        title: "View History",
         img: require('../src/assets/MainApp/E-Inventory/Sales.png')
     },
     {
